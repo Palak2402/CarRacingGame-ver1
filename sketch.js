3022a3e9cfd0aeea316e61b7fb85004931d7a2ab
Palak2402 <63538659+Palak2402@users.noreply.github.com>
2020-08-02 14:57:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas, backgroundImage, database;
+var gameState = 0 ;
+var playerCount;
+var form, player, game;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(400,400);
+   
+}
+
+function draw(){
+}
+
+
+ 
